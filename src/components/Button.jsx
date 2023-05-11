668943a7251590b2e2f98bc50c3f98b12a028aa2
Devin Latham <devinlatham20@gmail.com>
2023-05-11 15:55:21 -0500

    Finish business and billing section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+/* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
 
-const Button = () => {
-  return (
-    <div>Button</div>
+
+
+const Button = ({styles}) => (
+   <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] ${styles}`}>
+      Get Started
+   </button>
   )
-}
+
 
 export default Button
